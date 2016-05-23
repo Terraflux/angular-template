73628b5d8b4842a92febf6f597f3b7582ffd41b6
Pedro Urbina <pedro.urbina@gmail.com>
2016-05-23 19:04:45 -0400

    modals

--- dist/scripts/controllers/ModalCtrl.js
+++ dist/scripts/controllers/ModalCtrl.js
@@ -1,25 +1,43 @@
 (function () {
 	
-	function ModalCtrl($mdDialog, $mdMedia) {
+	function ModalCtrl($mdDialog, $mdMedia, $http, $mdToast) {
+
+		this.client = {
+			name: '',
+			email: '',
+			message: ''
+		};
 		
 		this.showContactPrompt = function($event) {
-			
-			var contactPrompt = $mdDialog.prompt()
-				.title('Contact Modal')
-				.textContent('Enter your message')
-				.placeholder('I need help with some issue')
-				.ariaLabel('Email message')
-				.targetEvent($event)
-				.ok('Send')
-				.cancel('Cancel')
-				.clickOutsideToClose(true);
 
-			$mdDialog.show(contactPrompt)
+			$mdDialog.show({
+				templateUrl: 'templates/contactModal.html',
+				clickOutsideToClose: true,
+				controller: 'ModalCtrl as modal',
+				targetEvent: $event
+			});
 		}
+
+		this.pushMessage = function(){
+
+			var data = this.client;
+
+			$http.post('/contact-form, data')
+				.success(function(data, status, headers, config) {
+					$mdToast.show(
+						$mdToast.simple()
+							.content('Thanks for your message' + data.name)
+							.hideDelay(5000)
+					);
+				}).
+				error(function(data, status, headers, config) {
+					console.log('error sending message');
+				});
+		};
 
 	};
 
 	angular
 		.module('fits')
-		.controller('ModalCtrl', ['$mdDialog', '$mdMedia', ModalCtrl]);
+		.controller('ModalCtrl', ['$mdDialog', '$mdMedia', '$http', '$mdToast', ModalCtrl,]);
 })();
